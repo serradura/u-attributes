2846fe7a8f5ddd407481530b96be6eb9b350532e
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-07-07 13:08:33 -0300

    Replace class_eval strategy by a module in Micro::Attributes.to_initialize()

---
+++ lib/micro/attributes/to_initialize.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Micro
+  module Attributes
+    module ToInitialize
+      def self.included(base)
+        base.send(:include, ::Micro::Attributes)
+      end
+
+      def initialize(arg)
+        self.attributes = arg
+      end
+
+      def with_attribute(key, val)
+        self.class.new(attributes.merge(key => val))
+      end
+
+      def with_attributes(arg)
+        self.class.new(attributes.merge(arg))
+      end
+    end
+
+    private_constant :ToInitialize
+  end
+end
