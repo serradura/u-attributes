bddf348e0be4dfc49ed03d9b5841fbd95da3feab
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-07-09 02:00:13 -0300

    Split tests into new files by context

---
+++ test/micro/attributes_after_inheritance_test.rb
@@ -0,0 +1,71 @@
+require "test_helper"
+
+class Micro::AttributesAfterInheritanceTest < Minitest::Test
+  class Base
+    include Micro::Attributes.to_initialize
+
+    attributes :e, f: 'ƒ'
+  end
+
+  class AnotherClassWithAttributes
+    include Micro::Attributes
+  end
+
+  def test_base_classes_cant_access_the_methods_to_override_attributes_data
+    [Base, AnotherClassWithAttributes].each do |klass|
+      refute klass.respond_to?(:attribute!, true)
+      refute klass.respond_to?(:attributes!, true)
+    end
+  end
+
+  class Sub < Base
+  end
+
+  def test_the_inheritance_of_default_values
+    assert_equal ['e', 'f'], Sub.attributes
+
+    object = Sub.new(e: '£')
+
+    assert_equal('£', object.e)
+    assert_equal('ƒ', object.f)
+  end
+
+  class SubSub < Sub
+    attribute! :f, 'F'
+  end
+
+  class SubSub2 < Sub
+    attribute! :h
+    attribute! :i, -99
+    attributes! e: '3', f: '_F_', g: 99
+  end
+
+  def test_overriding_default_attributes_data_with_subclasses
+    assert_equal(['e', 'f'], SubSub.attributes)
+    assert_equal(['e', 'f', 'h', 'i', 'g'], SubSub2.attributes)
+
+    assert_equal(Sub.attributes, SubSub.attributes)
+    refute_equal(Sub.attributes, SubSub2.attributes)
+
+    refute_equal(Sub.attributes_data({}), SubSub.attributes_data({}))
+    refute_equal(SubSub.attributes_data({}), SubSub2.attributes_data({}))
+
+    object1 = SubSub.new(e: 3)
+
+    assert_equal(3, object1.e)
+    assert_equal('F', object1.f)
+
+    object2 = SubSub2.new({})
+
+    assert_equal('3', object2.e)
+    assert_equal('_F_', object2.f)
+    assert_equal(99, object2.g)
+    assert_nil(object2.h)
+    assert_equal(-99, object2.i)
+  end
+
+  def test_the_argument_error_of_attributes!
+    error = assert_raises(ArgumentError) { SubSub.attributes! }
+    assert_equal('wrong number of arguments (given 0, expected 1 or more)', error.message)
+  end
+end
--- test/micro/attributes_test.rb
+++ test/micro/attributes_test.rb
@@ -1,12 +1,6 @@
 require "test_helper"
 
 class Micro::AttributesTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Micro::Attributes::VERSION
-  end
-
-  # ---
-
   class Biz
     include Micro::Attributes
 
@@ -53,7 +47,7 @@ class Micro::AttributesTest < Minitest::Test
     attribute 'c', 'C'
   end
 
-  def test_single_definition_with_default_values
+  def test_single_definitions_with_default_values
     object = Baz.new(a: 'a')
 
     assert_equal 'a', object.a
@@ -218,39 +212,6 @@ class Micro::AttributesTest < Minitest::Test
 
   # ---
 
-  def test_the_constructor_argument_validation
-    [Bar, Foo, Baz, Foz].each do |klass|
-      error = assert_raises(ArgumentError) { klass.new(1) }
-      assert_equal('argument must be a Hash', error.message)
-    end
-  end
-
-  # ---
-
-  def test_private_class_methods
-    [Bar, Foo, Baz, Foz].each do |klass|
-      assert klass.respond_to?(:__attributes_data, true)
-      assert_raises(NoMethodError) { klass.__attributes_data }
-
-      assert klass.respond_to?(:__attributes, true)
-      assert_raises(NoMethodError) { klass.__attributes }
-
-      assert klass.respond_to?(:__attributes_def, true)
-      assert_raises(NoMethodError) { klass.__attributes_def }
-
-      assert klass.respond_to?(:__attributes_set, true)
-      assert_raises(NoMethodError) { klass.__attributes_set }
-
-      assert klass.respond_to?(:__attribute_reader, true)
-      assert_raises(NoMethodError) { klass.__attribute_reader }
-
-      assert klass.respond_to?(:__attribute_set, true)
-      assert_raises(NoMethodError) { klass.__attribute_set }
-    end
-  end
-
-  # ---
-
   def test_attributes_data
     [Bar, Foo, Baz, Foz].each do |klass|
       error = assert_raises(ArgumentError) { klass.attributes_data(1) }
@@ -259,113 +220,5 @@ class Micro::AttributesTest < Minitest::Test
       assert klass.attributes_data({}).is_a?(Hash)
       refute klass.attributes_data({}).empty?
     end
-  end
-
-  # ---
-
-  def test_build_new_instance_after_set_one_attribute
-    bar1 = Bar.new(a: 'a')
-    bar2 = bar1.with_attribute(:a, 'A')
-    bar3 = bar1.with_attribute(:a, '@')
-
-    assert_equal('a', bar1.a)
-    assert_equal('A', bar2.a)
-    assert_equal('@', bar3.a)
-
-    refute bar1 == bar2
-    refute bar1 == bar3
-  end
-
-  # ---
-
-  def test_build_new_instance_after_set_many_attributes
-    baz1 = Baz.new(c: 'CC')
-    baz2 = baz1.with_attributes(a: 'A', b: :bb)
-    baz3 = baz1.with_attributes(a: '@', b: 'Bb')
-
-    assert_equal({'a' => nil, 'b' => "B", "c"=>"CC"}, baz1.attributes)
-    assert_equal({'a' => 'A', 'b' => :bb, "c"=>"CC"}, baz2.attributes)
-    assert_equal({'a' => '@', 'b' => 'Bb', "c"=>"CC"}, baz3.attributes)
-
-    refute_same baz1, baz2
-    refute_same baz1, baz3
-  end
-
-  # ---
-
-  class Base
-    include Micro::Attributes.to_initialize
-
-    attributes :e, f: 'ƒ'
-  end
-
-  def test_base_classes_cant_access_the_methods_to_override_attributes_data
-    [Bar, Foo, Baz, Foz, Base].each do |klass|
-      refute klass.respond_to?(:attribute!, true)
-      refute klass.respond_to?(:attributes!, true)
-    end
-  end
-
-  class Sub < Base
-  end
-
-  def test_inheritance
-    assert_equal ['e', 'f'], Sub.attributes
-
-    object = Sub.new(e: '£')
-
-    assert_equal('£', object.e)
-    assert_equal('ƒ', object.f)
-  end
-
-  class SubSub < Sub
-    attribute! :f, 'F'
-  end
-
-  class SubSub2 < Sub
-    attribute! :h
-    attribute! :i, -99
-    attributes! e: '3', f: '_F_', g: 99
-  end
-
-  def test_overriding_default_attributes_data_with_subclasses
-    assert_equal(['e', 'f'], SubSub.attributes)
-    assert_equal(['e', 'f', 'h', 'i', 'g'], SubSub2.attributes)
-
-    assert_equal(Sub.attributes, SubSub.attributes)
-    refute_equal(Sub.attributes, SubSub2.attributes)
-
-    refute_equal(Sub.attributes_data({}), SubSub.attributes_data({}))
-    refute_equal(SubSub.attributes_data({}), SubSub2.attributes_data({}))
-
-    object1 = SubSub.new(e: 3)
-
-    assert_equal(3, object1.e)
-    assert_equal('F', object1.f)
-
-    object2 = SubSub2.new({})
-
-    assert_equal('3', object2.e)
-    assert_equal('_F_', object2.f)
-    assert_equal(99, object2.g)
-    assert_nil(object2.h)
-    assert_equal(-99, object2.i)
-  end
-
-  def test_the_argument_error_of_attributes!
-    error = assert_raises(ArgumentError) { SubSub.attributes! }
-    assert_equal('wrong number of arguments (given 0, expected 1 or more)', error.message)
-  end
-
-  # ---
-
-  def test_private_constants
-    [Bar, Foo, Baz, Foz, Base, Sub, SubSub, SubSub2].each do |klass|
-      refute klass.constants.include?(:Macros)
-      assert klass.constants.include?(:AttributesUtils)
-    end
-
-    err = assert_raises(NameError) { Micro::Attributes::ToInitialize }
-    assert_equal('private constant Micro::Attributes::ToInitialize referenced', err.message)
   end
 end
