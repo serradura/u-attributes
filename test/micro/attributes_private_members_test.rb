f953bef8e9f9c0984914f162d48249318caed37a
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-07-09 02:00:13 -0300

    Split tests into new files by context

---
+++ test/micro/attributes_private_members_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class Micro::AttributesPrivateMembersTest < Minitest::Test
+  class A
+    include Micro::Attributes
+  end
+
+  class AA < A; end
+  class AAA < AA; end
+
+  class B
+    include Micro::Attributes.to_initialize
+  end
+
+  class BB < B; end
+  class BBB < BB; end
+
+  def test_private_class_methods
+    [A, AA, AAA, B, BB, BBB].each do |klass|
+      assert klass.respond_to?(:__attributes_data, true)
+      assert_raises(NoMethodError) { klass.__attributes_data }
+
+      assert klass.respond_to?(:__attributes, true)
+      assert_raises(NoMethodError) { klass.__attributes }
+
+      assert klass.respond_to?(:__attributes_def, true)
+      assert_raises(NoMethodError) { klass.__attributes_def }
+
+      assert klass.respond_to?(:__attributes_set, true)
+      assert_raises(NoMethodError) { klass.__attributes_set }
+
+      assert klass.respond_to?(:__attribute_reader, true)
+      assert_raises(NoMethodError) { klass.__attribute_reader }
+
+      assert klass.respond_to?(:__attribute_set, true)
+      assert_raises(NoMethodError) { klass.__attribute_set }
+    end
+  end
+
+  def test_private_constants
+    [A, AA, AAA, B, BB, BBB].each do |klass|
+      refute klass.constants.include?(:Macros)
+      assert klass.constants.include?(:AttributesUtils)
+    end
+  end
+end
---
+++ test/micro/attributes_to_initialize_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class Micro::AttributesToInitializeTest < Minitest::Test
+  class Foo
+    include Micro::Attributes::ToInitialize
+
+    attribute :a
+    attribute 'b'
+  end
+
+  class Bar
+    include Micro::Attributes.to_initialize
+
+    attribute :a
+    attribute :b, 'B'
+    attribute 'c', 'C'
+  end
+
+  def test_the_constructor_argument_validation
+    [Foo, Bar].each do |klass|
+      error = assert_raises(ArgumentError) { klass.new(1) }
+      assert_equal('argument must be a Hash', error.message)
+    end
+  end
+
+  def test_build_new_instance_after_set_one_attribute
+    instance_1 = Foo.new(a: 'a')
+    instance_2 = instance_1.with_attribute(:a, 'A')
+    instance_3 = instance_1.with_attribute(:a, '@')
+
+    assert_equal('a', instance_1.a)
+    assert_equal('A', instance_2.a)
+    assert_equal('@', instance_3.a)
+
+    refute_same(instance_1, instance_2)
+    refute_same(instance_1, instance_3)
+  end
+
+  def test_build_new_instance_after_set_many_attributes
+    instance_1 = Bar.new(c: 'CC')
+    instance_2 = instance_1.with_attributes(a: 'A', b: :bb)
+    instance_3 = instance_1.with_attributes(a: '@', b: 'Bb')
+
+    assert_equal({'a' => nil, 'b' => "B", "c"=>"CC"}, instance_1.attributes)
+    assert_equal({'a' => 'A', 'b' => :bb, "c"=>"CC"}, instance_2.attributes)
+    assert_equal({'a' => '@', 'b' => 'Bb', "c"=>"CC"}, instance_3.attributes)
+
+    refute_same(instance_1, instance_2)
+    refute_same(instance_1, instance_3)
+  end
+end
