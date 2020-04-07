7056f18935f621aed9c09405d88be6d27da50a49
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-04-07 00:51:52 -0300

    Setup test suite to run with Ruby 2.7

--- .travis.sh
+++ .travis.sh
@@ -20,5 +20,12 @@ ACTIVEMODEL_VERSION='5.0' bundle exec rake test
 ACTIVEMODEL_VERSION='5.1' bundle update
 ACTIVEMODEL_VERSION='5.1' bundle exec rake test
 
-ACTIVEMODEL_VERSION='5.2' bundle update
-ACTIVEMODEL_VERSION='5.2' bundle exec rake test
+if [[ ! $ruby_v =~ '2.2.0' ]]; then
+  ACTIVEMODEL_VERSION='5.2' bundle update
+  ACTIVEMODEL_VERSION='5.2' bundle exec rake test
+fi
+
+if [[ $ruby_v =~ '2.5.' ]] || [[ $ruby_v =~ '2.6.' ]] || [[ $ruby_v =~ '2.7.' ]]; then
+  ACTIVEMODEL_VERSION='6.0' bundle update
+  ACTIVEMODEL_VERSION='6.0' bundle exec rake test
+fi
