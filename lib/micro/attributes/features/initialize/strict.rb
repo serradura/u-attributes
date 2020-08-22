0374d2acb4ac1f0d2393869cf00f87d6c6778650
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-08-22 08:53:14 -0300

    Optimize Features::Initialize::Strict

--- lib/micro/attributes/features/initialize/strict.rb
+++ lib/micro/attributes/features/initialize/strict.rb
@@ -8,28 +8,26 @@ module Micro::Attributes
         MISSING_KEYWORDS = 'missing keywords'.freeze
 
         protected def attributes=(arg)
-          arg_hash = Utils.stringify_hash_keys(arg)
-          att_data = self.class.__attributes_data__
+          hash = Utils.stringify_hash_keys(arg)
 
-          attributes_missing!(ref: att_data, arg: arg_hash)
+          __attributes_missing!(hash)
 
-          __attributes_assign(arg_hash, att_data)
+          __attributes_assign(hash)
         end
 
-        private def attributes_missing!(ref:, arg:)
-          missing_keys = attributes_missing(ref, arg)
+        private def __attributes_missing!(hash)
+          required_keys = self.class.__attribute_names_without_default__
+
+          return if required_keys.empty?
+
+          missing_keys = required_keys.map { |name| ":#{name}" if !hash.key?(name) }
+          missing_keys.compact!
 
           return if missing_keys.empty?
 
           label = missing_keys.size == 1 ? MISSING_KEYWORD : MISSING_KEYWORDS
 
           raise ArgumentError, "#{label}: #{missing_keys.join(', ')}"
-        end
-
-        private def attributes_missing(ref, arg)
-          ref.each_with_object([]) do |(key, val), memo|
-            memo << ":#{key}" if val.nil? && !arg.has_key?(key)
-          end
         end
 
         private_constant :MISSING_KEYWORD, :MISSING_KEYWORDS
