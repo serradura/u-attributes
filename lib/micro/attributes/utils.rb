e79ea5d00ce3d7e96d38895f7d17883b11c8f82b
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-09-02 15:15:26 -0300

    Prefer reader methods into Micro::Attributes#extract_attributes_from

--- lib/micro/attributes/utils.rb
+++ lib/micro/attributes/utils.rb
@@ -1,25 +1,36 @@
 # frozen_string_literal: true
 
-module Micro
-  module Attributes
-    module Utils
-      def self.stringify_hash_keys(arg)
+module Micro::Attributes
+  module Utils
+    module Hashes
+      def self.stringify_keys(arg)
         hash = Kind::Of.(::Hash, arg)
 
         return hash if hash.empty?
+        return hash.transform_keys(&:to_s) if hash.respond_to?(:transform_keys)
 
-        if hash.respond_to?(:transform_keys)
-          hash.transform_keys { |key| key.to_s }
-        else
-          hash.each_with_object({}) { |(key, val), memo| memo[key.to_s] = val }
-        end
+        hash.each_with_object({}) { |(key, val), memo| memo[key.to_s] = val }
       end
 
-      HashAccess = -> (hash, key) {
-        return hash[key.to_s] unless hash[key.to_s].nil?
+      def self.get(hash, key)
+        value = hash[key.to_s]
 
-        hash[key.to_sym]
-      }
+        value.nil? ? hash[key.to_sym] : value
+      end
+    end
+
+    module ExtractAttribute
+      def self.call(object, key:)
+        return object.public_send(key) if object.respond_to?(key)
+
+        Hashes.get(object, key) if object.respond_to?(:[])
+      end
+
+      def self.from(object, keys:)
+        Kind::Of.(::Array, keys).each_with_object({}) do |key, memo|
+          memo[key] = call(object, key: key)
+        end
+      end
     end
   end
 end
