5211b915f23ebdc5b6b840c459bd3229f2ed07d0
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-09-06 21:30:29 -0300

    Add options to slice attributes

--- test/micro/attributes/utils_test.rb
+++ test/micro/attributes/utils_test.rb
@@ -53,6 +53,39 @@ class Micro::Attributes::UtilsTest < Minitest::Test
     end
   end
 
+  def test_the_transformation_of_hash_keys
+    err1 = assert_raises(Kind::Error) { Micro::Attributes::Utils::Hashes.keys_as(nil, []) }
+
+    assert_equal('[] expected to be a kind of Hash', err1.message)
+
+    # --
+
+    err2 = assert_raises(Kind::Error) { Micro::Attributes::Utils::Hashes.keys_as(Symbol, []) }
+    err3 = assert_raises(Kind::Error) { Micro::Attributes::Utils::Hashes.keys_as(String, []) }
+
+    assert_equal('[] expected to be a kind of Hash', err2.message)
+    assert_equal('[] expected to be a kind of Hash', err3.message)
+
+    # --
+
+    hash1 = { :a => 1 }
+    hash2 = { 'a' => 1 }
+
+    err4 = assert_raises(ArgumentError) { Micro::Attributes::Utils::Hashes.keys_as(Hash, hash1) }
+    err5 = assert_raises(ArgumentError) { Micro::Attributes::Utils::Hashes.keys_as(Array, hash2) }
+
+    assert_equal('first argument must be the class String or Symbol', err4.message)
+    assert_equal('first argument must be the class String or Symbol', err5.message)
+
+    # --
+
+    assert_equal({:a => 1}, Micro::Attributes::Utils::Hashes.keys_as(Symbol, hash1))
+    assert_equal({:a => 1}, Micro::Attributes::Utils::Hashes.keys_as(Symbol, hash2))
+
+    assert_equal({'a' => 1}, Micro::Attributes::Utils::Hashes.keys_as(String, hash1))
+    assert_equal({'a' => 1}, Micro::Attributes::Utils::Hashes.keys_as(String, hash2))
+  end
+
   def test_allow_hash_access_with_string_or_symbol_keys
     hash = { :symbol => 'symbol', 'string' => 'string', false: false }
 
